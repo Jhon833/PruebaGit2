4282d9d988fdff4ed7ad0583eb6140da52d31406
Jhon833 <130400216+Jhon833@users.noreply.github.com>
2023-04-12 15:39:22 -0300

    Update PruebaGit.java

--- src/Git/PruebaGit.java
+++ src/Git/PruebaGit.java
@@ -4,6 +4,11 @@ public class PruebaGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		int a = 1;
+		int b = 2;
+		int promedio = (a+b)/2;
+		System.out.println(promedio);
 
 	}
 
